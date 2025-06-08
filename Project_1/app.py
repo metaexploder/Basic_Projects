41aa782d6db5d5dd3a9817db38dfa35f44be32c3
metaexploder <vishal.official2219@gmail.com>
2025-06-08 11:47:59 +0530

    declaring mulitple data set for authenication in flask app

--- Project_1/app.py
+++ Project_1/app.py
@@ -10,7 +10,13 @@ def submit():
     username = request.form.get("username") #request jo form m aayi h usse get kr lo username jo aaya h 
     password = request.form.get("password")
 
-    if username == "admin" and password == "admin":
+    users = {
+        "vishal" : "123",
+        "admin" : "456",
+        "gaurav" : "789"
+    }
+
+    if username in users and password == users[username]:
         return render_template("welcome.html", name = username)
     else:
         return Response("In-valid credintials. Try agian", mimetype = "text/plain")
